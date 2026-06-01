9882f25d8ab87ff03fb4ce3fed485ecb073e0cb0
Marcos Bulcao <bulcaoacademy@gmail.com>
2026-06-01 06:54:40 +0000

    feat: add interactive book prototype at book.smartlogtrading.com

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/:path*',
+          has: [{ type: 'host', value: 'book.smartlogtrading.com' }],
+          destination: '/book.html',
+        },
+      ],
+    };
+  },
 };
 
 export default nextConfig;
